d88444a707ba2668203c18bae3a4879fefeb0351
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-08-05 19:22:59 +0200

    change const randomDestruction and randomLucky

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 'use strict';
 const title = document.querySelector('.title'); 
 
+function getRandomNumber(max) {
+return Math.ceil(Math.random() * max);
+}
+
 if (localStorage.getItem('thanosDecision')) {
   title.innerHTML = localStorage.getItem('thanosDecision');
 }
 else {
-  function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-  }
   let thanosRandom = getRandomNumber(50);
-
-  if (thanosRandom<25) {const randomDestruction = title.innerHTML = `You were slain by Thanos, for the good of the Universe.`;
-localStorage.setItem('thanosDecision', randomDestruction)
-}
-else {
-  const randomLucky = title.innerHTML = `You were spared by Thanos.`;
+  if (thanosRandom<25) {
+  const randomDestruction = `You were slain by Thanos, for the good of the Universe.`;
+  title.innerHTML = randomDestruction;
+  localStorage.setItem('thanosDecision', randomDestruction)
+  }
+  else {
+  const randomLucky = `You were spared by Thanos.`;
+  title.innerHTML = randomLucky;
   localStorage.setItem('thanosDecision', randomLucky)
-}
-}
+  }
+  }
 
 
